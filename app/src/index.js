23b021fef398f36fb18a4760b833b2bc8c5fcbff
MOURCHID Mohamed Kamal <mmourchid@scdev.ma>
2026-06-21 00:20:33 +0100

    fixing datetime picker bug

--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import dayjs from 'dayjs';
+import localeData from 'dayjs/plugin/localeData';
+import weekday from 'dayjs/plugin/weekday';
+import 'dayjs/locale/en';
 import App from './App';
 import './index.css';
+
+dayjs.extend(localeData);
+dayjs.extend(weekday);
+dayjs.locale('en');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
